a6c8be75bbf41c8a65a21583f7cc09491bac2d8c
tarifalhasan <tarifalhasan999@gmail.com>
2024-02-12 15:01:58 +0600

    header hero completed

---
+++ components/Landing/Hero.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectFade, Pagination } from "swiper/modules";
+import { Button } from "../ui/button";
+
+export default function Hero() {
+  const data = [
+    {
+      imageUrl: "/images/hero/1.png",
+      title: "SUNMI OFFICIAL THAILAND",
+      description:
+        "SUNMI OFFICIAL เป็นผู้นำเข้าและจัดจำหน่ายสินค้า อย่างเป็นทางการของแบรนด์ SUNMI ทางเรามีทีมงานผู้เชี่ยวชาญ ที่ได้รับการฝึกอบรมมาอย่างมืออาชีพ",
+    },
+    {
+      imageUrl: "/images/hero/1.png",
+      title: "SUNMI OFFICIAL THAILAND",
+      description:
+        "SUNMI OFFICIAL เป็นผู้นำเข้าและจัดจำหน่ายสินค้า อย่างเป็นทางการของแบรนด์ SUNMI ทางเรามีทีมงานผู้เชี่ยวชาญ ที่ได้รับการฝึกอบรมมาอย่างมืออาชีพ",
+    },
+    {
+      imageUrl: "/images/hero/1.png",
+      title: "SUNMI OFFICIAL THAILAND",
+      description:
+        "SUNMI OFFICIAL เป็นผู้นำเข้าและจัดจำหน่ายสินค้า อย่างเป็นทางการของแบรนด์ SUNMI ทางเรามีทีมงานผู้เชี่ยวชาญ ที่ได้รับการฝึกอบรมมาอย่างมืออาชีพ",
+    },
+    {
+      imageUrl: "/images/hero/1.png",
+      title: "SUNMI OFFICIAL THAILAND",
+      description:
+        "SUNMI OFFICIAL เป็นผู้นำเข้าและจัดจำหน่ายสินค้า อย่างเป็นทางการของแบรนด์ SUNMI ทางเรามีทีมงานผู้เชี่ยวชาญ ที่ได้รับการฝึกอบรมมาอย่างมืออาชีพ",
+    },
+  ];
+
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Pagination]}
+        className="heroSwiper"
+      >
+        {data.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                backgroundImage: `url('${item.imageUrl}')`,
+                backgroundSize: "100% 100%",
+              }}
+              className="lg:min-h-[99vh] bg-no-repeat"
+            >
+              <div className="container relative mx-auto py-28 md:py-48 px-4 w-full left-0 top-0">
+                <div className="space-y-7">
+                  <h2 className="font-bold text-center font-sans lg:text-left text-2xl sm:text-4xl xl:text-5xl">
+                    {item.title}
+                  </h2>
+                  <p className="max-w-[542px] mx-auto lg:mx-0 font-sans text-lg text-center lg:text-left xl:text-xl font-bold">
+                    {item.description}
+                  </p>
+                  <div className="flex justify-center lg:justify-start">
+                    <Button className="px-6 rounded-full bg-white text-primary hover:text-foreground lg:bg-primary lg:text-white lg:hover:text-white">
+                      เรียนรู้เพิ่มเติม
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
---
+++ components/layout/Header.tsx
@@ -0,0 +1,277 @@
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { HiOutlineMenu } from "react-icons/hi";
+import { IoClose, IoNotifications } from "react-icons/io5";
+import { MdShoppingCart } from "react-icons/md";
+import SearchBar from "../ui/searchbar";
+
+const MainHeader = () => {
+  const [header, setHeader] = useState(false);
+  const [nav, setNav] = useState(false);
+  const [language, setLanguage] = useState("ไทย");
+  const Links = [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Products ",
+      href: "/products",
+    },
+    {
+      label: "Shop",
+      href: "/shop",
+    },
+    {
+      label: "About us",
+      href: "/about_us",
+    },
+    {
+      label: "Contact us",
+      href: "/contact_us",
+    },
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 40) {
+        setHeader(true);
+      } else {
+        setHeader(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const onLanguagesChanges = (value: string) => {
+    setLanguage(value);
+  };
+  return (
+    <header
+      className={cn(
+        !header
+          ? " bg-transparent py-4 shadow-none  "
+          : " shadow-none border-b backdrop-blur-sm bg-white/[0.6] dark:bg-black/[0.6] border-neutral-200 border-white/[0.1] ",
+        "  top-0 w-full mx-auto z-40 py-5 fixed transition-all duration-300"
+      )}
+    >
+      <div className=" px-4 xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
+        <div className="flex gap-x-6 items-center justify-between w-full">
+          <div className=" inline-flex items-center gap-x-20">
+            <Link href={"/"}>
+              <Image
+                src={"/images/Shanghai-Sunmi-Technology-LOGO.svg"}
+                alt="Shanghai-Sunmi-Technology-LOGO"
+                width={139}
+                height={26}
+              />
+            </Link>
+            <ul
+              className={cn(
+                nav ? " py-8 xl:py-0  xl:px-0" : "max-h-0 xl:max-h-max w-full",
+                "  hidden xl:flex flex-col items-center w-full  xl:flex-row xl:w-max xl:gap-x-8 gap-y-5"
+              )}
+            >
+              {Links.map((link, index) => (
+                <li
+                  className={cn(
+                    "relative text-base font-medium  text-skin-neutral-600 dark:text-white/95 "
+                  )}
+                  key={index}
+                >
+                  <Link href={link.href}>{link.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="hidden xl:inline-flex items-center gap-5">
+            <div className=" inline-flex items-center gap-x-5 ">
+              <SearchBar />
+              <button className=" relative ">
+                <MdShoppingCart className=" w-7 h-7 text-primary" />
+                <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
+                  1
+                </span>
+              </button>
+              <button className=" relative ">
+                <IoNotifications className=" w-7 h-7 text-primary" />
+                <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
+                  1
+                </span>
+              </button>
+            </div>
+            <div className=" w-[1px] bg-[#DAF1E9] h-[40px]"></div>
+            <div className=" inline-flex items-center gap-5">
+              <Select
+                value={language}
+                onValueChange={(value) => onLanguagesChanges(value)}
+              >
+                <SelectTrigger className=" w-[100px] bg-transparent border-none focus:outline-none focus:ring-0 focus:ring-offset-0 ">
+                  <div className=" inline-flex items-center gap-4">
+                    <Avatar>
+                      <AvatarImage
+                        src={
+                          language === "en"
+                            ? "/icons/Flag_of_the_United_States_(DoS_ECA_Color_Standard).svg.png"
+                            : "/icons/Flag_of_Thailand.png"
+                        }
+                      />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <span className=" uppercase text-base font-normal text-skin-neutral-600">
+                      {language}
+                    </span>
+                  </div>
+                </SelectTrigger>
+                <SelectContent align="end">
+                  <SelectItem value="en">Eng</SelectItem>
+                  <SelectItem value="ไทย">ไทย</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Billing</DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Logout</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+          <div
+            className=" xl:cursor-pointer xl:hidden"
+            onClick={() => setNav((prev) => !prev)}
+          >
+            {nav ? (
+              <IoClose className=" w-6 h-6 text-black dark:text-white/95 " />
+            ) : (
+              <HiOutlineMenu className=" w-6 h-6 text-black dark:text-white/95 " />
+            )}
+          </div>
+        </div>
+        {nav && (
+          <ul
+            className={cn(
+              nav
+                ? " py-8 xl:py-0  xl:px-0"
+                : "max-h-0 xl:max-h-max w-full bg-background",
+              " flex  flex-col items-center w-full bg-background/90  xl:flex-row xl:w-max xl:gap-x-8 gap-y-5"
+            )}
+          >
+            {Links.map((link, index) => (
+              <li
+                className={cn(
+                  "relative text-base font-medium  text-skin-neutral-600 dark:text-white/95 "
+                )}
+                key={index}
+              >
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+            <li>
+              <div className=" xl:hidden flex flex-col items-center gap-5">
+                <div className=" inline-flex items-center gap-x-5 ">
+                  <SearchBar />
+                  <button className=" relative ">
+                    <MdShoppingCart className=" w-7 h-7 text-primary" />
+                    <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
+                      1
+                    </span>
+                  </button>
+                  <button className=" relative ">
+                    <IoNotifications className=" w-7 h-7 text-primary" />
+                    <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
+                      1
+                    </span>
+                  </button>
+                </div>
+                <div className=" w-[1px] bg-[#DAF1E9] h-[40px]"></div>
+                <div className=" inline-flex items-center gap-5">
+                  <Select
+                    value={language}
+                    onValueChange={(value) => onLanguagesChanges(value)}
+                  >
+                    <SelectTrigger className=" w-[100px] bg-transparent border-none focus:outline-none focus:ring-0 focus:ring-offset-0 ">
+                      <div className=" inline-flex items-center gap-4">
+                        <Avatar>
+                          <AvatarImage
+                            src={
+                              language === "en"
+                                ? "/icons/Flag_of_the_United_States_(DoS_ECA_Color_Standard).svg.png"
+                                : "/icons/Flag_of_Thailand.png"
+                            }
+                          />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <span className=" uppercase text-base font-normal text-skin-neutral-600">
+                          {language}
+                        </span>
+                      </div>
+                    </SelectTrigger>
+                    <SelectContent align="end">
+                      <SelectItem value="en">Eng</SelectItem>
+                      <SelectItem value="ไทย">ไทย</SelectItem>
+                    </SelectContent>
+                  </Select>
+
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Profile</DropdownMenuItem>
+                      <DropdownMenuItem>Billing</DropdownMenuItem>
+                      <DropdownMenuItem>Team</DropdownMenuItem>
+                      <DropdownMenuItem>Logout</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              </div>
+            </li>
+          </ul>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default MainHeader;

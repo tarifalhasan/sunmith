e1f183dddc98f7b932389d6f0f8a37cfa1f62e0a
tarifalhasan <tarifalhasan999@gmail.com>
2024-02-17 21:43:51 +0600

    video player added

--- components/Landing/Services.tsx
+++ components/Landing/Services.tsx
@@ -3,6 +3,7 @@ import { useLocale, useTranslations } from "next-intl";
 import Image from "next/image";
 import { IoChevronForward } from "react-icons/io5";
 import { Button } from "../ui/button";
+import VidePlay from "./VidePlay";
 const Services = () => {
   const t = useTranslations("Services");
   const local = useLocale(); // local return th and en value
@@ -25,43 +26,10 @@ const Services = () => {
           </div>
         </div>
       </div>
-      <div
-        style={{
-          backgroundImage: "url('/images/screen-4-content-1 3.png')",
-          backgroundPosition: "100% 100%",
-        }}
-        className=" bg-cover my-10 relative overflow-hidden   min-h-[180px] sm:min-h-[350px] lg:min-h-[592px]"
-      >
-        <div className="  z-50  cursor-pointer absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Image
-            src={"/icons/play.svg"}
-            alt="play_video"
-            className=" max-w-[40%] sm:max-w-[70%] xl:max-w-[95%] block mx-auto"
-            width={144}
-            height={144}
-          />
-        </div>
-        <div className=" w-full h-full  z-10  absolute overflow-hidden hidden  lg:block">
-          <div className=" w-full h-full overflow-hidden relative container ">
-            <div className=" space-y-4  mt-10  absolute left-4 bottom-12   max-w-[400px] xl:max-w-[535px]  rounded-[16px] p-4 bg-white/60">
-              <div>
-                <h2 className=" text-center sm:text-left text-2xl text-primary font-sans font-bold">
-                  SUNMI V2
-                </h2>
-              </div>
-              <p className=" text-center sm:text-left font-sans font-normal text-base">
-                {local === "en"
-                  ? "As the official importer and distributor of SUNMI brand products, we have a professional team that has received extensive training to provide guidance, usage instructions, and post-sales consultation services to troubleshoot product-related issues. Our aim is to help your business run smoothly and deliver the best service to our customers nationwide. SUNMI's innovative products meet global standards."
-                  : "เป็นผู้นำเข้าและจัดจำหน่ายสินค้าอย่างเป็นทางการ ของแบรนด์ SUNMI ทางเรามีทีมงานผู้เชี่ยวชาญ ที่ได้รับการฝึกอบรมมาอย่างมืออาชีพให้คำแนะนำ การใช้งานและบริการให้คำปรึกษาหลังการขายในการ แก้ไขปัญหา การใช้งานสินค้า เพื่อช่วยให้ธุรกิจของท่าน ดำเนินไปได้ อย่างราบรื่นและมอบการบริการที่ดีที่สุด ให้แก่ลูกค้าของเราจากทั่วทั้งประเทศ สินค้านวัตกรรม ของทาง SUNMI ได้รับมาตรฐานระดับโลก"}
-              </p>
-              <Button className=" gap-2 hover:gap-x-4 transition-all duration-300 rounded-full px-5">
-                {local === "en" ? "Buy now" : "ซื้อเลย "}
-                <IoChevronForward className=" w-4 h-4 text-white" />
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* Video Player  */}
+
+      <VidePlay />
+
       <div className=" space-y-4  flex flex-col items-center mt-10  lg:hidden   lg:max-w-[535px]  rounded-[16px] p-4 bg-white/60">
         <div>
           <h2 className=" text-center sm:text-left text-2xl text-primary font-sans font-bold">
@@ -69,14 +37,9 @@ const Services = () => {
           </h2>
         </div>
         <p className=" text-center sm:text-left font-sans font-normal text-base">
-          เป็นผู้นำเข้าและจัดจำหน่ายสินค้าอย่างเป็นทางการ ของแบรนด์ SUNMI
-          ทางเรามีทีมงานผู้เชี่ยวชาญ
-          ที่ได้รับการฝึกอบรมมาอย่างมืออาชีพให้คำแนะนำ
-          การใช้งานและบริการให้คำปรึกษาหลังการขายในการ แก้ไขปัญหา
-          การใช้งานสินค้า เพื่อช่วยให้ธุรกิจของท่าน ดำเนินไปได้
-          อย่างราบรื่นและมอบการบริการที่ดีที่สุด
-          ให้แก่ลูกค้าของเราจากทั่วทั้งประเทศ สินค้านวัตกรรม ของทาง SUNMI
-          ได้รับมาตรฐานระดับโลก
+          {local === "en"
+            ? "As the official importer and distributor of SUNMI brand products, we have a professional team that has received extensive training to provide guidance, usage instructions, and post-sales consultation services to troubleshoot product-related issues. Our aim is to help your business run smoothly and deliver the best service to our customers nationwide. SUNMI's innovative products meet global standards."
+            : "เป็นผู้นำเข้าและจัดจำหน่ายสินค้าอย่างเป็นทางการ ของแบรนด์ SUNMI ทางเรามีทีมงานผู้เชี่ยวชาญ ที่ได้รับการฝึกอบรมมาอย่างมืออาชีพให้คำแนะนำ การใช้งานและบริการให้คำปรึกษาหลังการขายในการ แก้ไขปัญหา การใช้งานสินค้า เพื่อช่วยให้ธุรกิจของท่าน ดำเนินไปได้ อย่างราบรื่นและมอบการบริการที่ดีที่สุด ให้แก่ลูกค้าของเราจากทั่วทั้งประเทศ สินค้านวัตกรรม ของทาง SUNMI ได้รับมาตรฐานระดับโลก"}
         </p>
         <Button className=" gap-2 hover:gap-x-4 transition-all duration-300 rounded-full px-5">
           ซื้อเลย <IoChevronForward className=" w-4 h-4 text-white" />
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -54,30 +54,86 @@ const MainHeader = () => {
   });
 
   return (
-    <header
-      className={cn(
-        !header || nav
-          ? " bg-transparent py-4 shadow-none  "
-          : " shadow-none border-b backdrop-blur-sm bg-white/[0.8] dark:bg-black/[0.6] border-neutral-200 border-white/[0.1] ",
-        "  top-0 w-full mx-auto z-40 py-5 fixed transition-all duration-300 h-[81px] overflow-hidden",
-        nav && "bg-white/[0.9]"
-      )}
-    >
-      <div className=" px-4 xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
-        <div className="flex gap-x-6 items-center justify-between w-full">
-          <div className=" inline-flex items-center gap-x-20">
-            <Link href={"/"}>
-              <Image
-                src={"/images/Shanghai-Sunmi-Technology-LOGO.svg"}
-                alt="Shanghai-Sunmi-Technology-LOGO"
-                width={139}
-                height={26}
-              />
-            </Link>
+    <header className=" relative w-full overflow-hidden">
+      <div
+        className={cn(
+          !header || nav
+            ? " bg-transparent py-4 shadow-none  "
+            : " shadow-none border-b backdrop-blur-sm bg-white/[0.8] dark:bg-black/[0.6] border-neutral-200 border-white/[0.1] ",
+          "  top-0 w-full mx-auto z-[9999] fixed py-5  transition-all duration-300 h-[81px] ",
+          nav && "bg-white/[0.9]"
+        )}
+      >
+        <div className=" px-4 xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
+          <div className="flex gap-x-6 items-center justify-between w-full">
+            <div className=" inline-flex items-center gap-x-20">
+              <Link href={"/"}>
+                <Image
+                  src={"/images/Shanghai-Sunmi-Technology-LOGO.svg"}
+                  alt="Shanghai-Sunmi-Technology-LOGO"
+                  width={139}
+                  height={26}
+                />
+              </Link>
+              <ul
+                className={cn(
+                  nav
+                    ? " py-8 xl:py-0  xl:px-0"
+                    : "max-h-0 xl:max-h-max w-full",
+                  "  hidden xl:flex flex-col items-center w-full  xl:flex-row xl:w-max xl:gap-x-8 gap-y-5"
+                )}
+              >
+                {Links.map((link, index) => (
+                  <li
+                    className={cn(
+                      "relative text-base font-medium  text-skin-neutral-600 dark:text-white/95 "
+                    )}
+                    key={index}
+                  >
+                    <Link href={link.href}>{link.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="hidden xl:inline-flex items-center gap-5">
+              <div className=" inline-flex items-center gap-x-5 ">
+                <SearchBar />
+                <button className=" relative ">
+                  <MdShoppingCart className=" w-7 h-7 text-primary" />
+                  <span className=" w-4 h-4 rounded-full bg-white absolute right-0 flex items-center text-[10px] font-roboto font-medium  text-skin-status-600 justify-center -top-1">
+                    01
+                  </span>
+                </button>
+                <button className=" relative ">
+                  <IoNotifications className=" w-7 h-7 text-primary" />
+                  <span className=" w-4 h-4 rounded-full bg-white absolute right-0 flex items-center  font-roboto font-medium text-[10px] text-skin-status-600  justify-center -top-1">
+                    01
+                  </span>
+                </button>
+              </div>
+              <div className=" w-[1px] bg-[#DAF1E9] h-[40px]"></div>
+              <div className=" inline-flex items-center gap-5">
+                <LocaleSwitcher />
+              </div>
+            </div>
+            <div
+              className=" xl:cursor-pointer xl:hidden"
+              onClick={() => setNav((prev) => !prev)}
+            >
+              {nav ? (
+                <IoClose className=" w-6 h-6 text-primary dark:text-white/95 " />
+              ) : (
+                <HiOutlineMenu className=" w-6 h-6 text-primary dark:text-white/95 " />
+              )}
+            </div>
+          </div>
+          {nav && (
             <ul
               className={cn(
-                nav ? " py-8 xl:py-0  xl:px-0" : "max-h-0 xl:max-h-max w-full",
-                "  hidden xl:flex flex-col items-center w-full  xl:flex-row xl:w-max xl:gap-x-8 gap-y-5"
+                nav
+                  ? " py-8 xl:py-0  xl:px-0"
+                  : "max-h-0 xl:max-h-max w-full bg-background",
+                " flex  flex-col items-center w-full bg-background/90  xl:flex-row xl:w-max xl:gap-x-8 gap-y-5"
               )}
             >
               {Links.map((link, index) => (
@@ -90,84 +146,32 @@ const MainHeader = () => {
                   <Link href={link.href}>{link.label}</Link>
                 </li>
               ))}
-            </ul>
-          </div>
-          <div className="hidden xl:inline-flex items-center gap-5">
-            <div className=" inline-flex items-center gap-x-5 ">
-              <SearchBar />
-              <button className=" relative ">
-                <MdShoppingCart className=" w-7 h-7 text-primary" />
-                <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
-                  1
-                </span>
-              </button>
-              <button className=" relative ">
-                <IoNotifications className=" w-7 h-7 text-primary" />
-                <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
-                  1
-                </span>
-              </button>
-            </div>
-            <div className=" w-[1px] bg-[#DAF1E9] h-[40px]"></div>
-            <div className=" inline-flex items-center gap-5">
-              <LocaleSwitcher />
-            </div>
-          </div>
-          <div
-            className=" xl:cursor-pointer xl:hidden"
-            onClick={() => setNav((prev) => !prev)}
-          >
-            {nav ? (
-              <IoClose className=" w-6 h-6 text-primary dark:text-white/95 " />
-            ) : (
-              <HiOutlineMenu className=" w-6 h-6 text-primary dark:text-white/95 " />
-            )}
-          </div>
-        </div>
-        {nav && (
-          <ul
-            className={cn(
-              nav
-                ? " py-8 xl:py-0  xl:px-0"
-                : "max-h-0 xl:max-h-max w-full bg-background",
-              " flex  flex-col items-center w-full bg-background/90  xl:flex-row xl:w-max xl:gap-x-8 gap-y-5"
-            )}
-          >
-            {Links.map((link, index) => (
-              <li
-                className={cn(
-                  "relative text-base font-medium  text-skin-neutral-600 dark:text-white/95 "
-                )}
-                key={index}
-              >
-                <Link href={link.href}>{link.label}</Link>
+              <li>
+                <div className=" xl:hidden flex flex-col items-center gap-5">
+                  <div className=" inline-flex items-center gap-x-5 ">
+                    <SearchBar />
+                    <button className=" relative ">
+                      <MdShoppingCart className=" w-7 h-7 text-primary" />
+                      <span className=" w-4 h-4 rounded-full bg-white absolute right-0 flex items-center text-[10px]  text-skin-status-600  justify-center font-medium font-roboto -top-1">
+                        01
+                      </span>
+                    </button>
+                    <button className=" relative ">
+                      <IoNotifications className=" w-7 h-7 text-primary" />
+                      <span className=" w-4 h-4 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-roboto font-medium justify-center -top-1">
+                        01
+                      </span>
+                    </button>
+                  </div>
+                  <div className=" w-[1px] bg-[#DAF1E9] h-[40px]"></div>
+                  <div className=" inline-flex items-center gap-5">
+                    <LocaleSwitcher />
+                  </div>
+                </div>
               </li>
-            ))}
-            <li>
-              <div className=" xl:hidden flex flex-col items-center gap-5">
-                <div className=" inline-flex items-center gap-x-5 ">
-                  <SearchBar />
-                  <button className=" relative ">
-                    <MdShoppingCart className=" w-7 h-7 text-primary" />
-                    <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
-                      1
-                    </span>
-                  </button>
-                  <button className=" relative ">
-                    <IoNotifications className=" w-7 h-7 text-primary" />
-                    <span className=" w-3 h-3 rounded-full bg-white absolute right-0 flex items-center text-[10px] text-skin-status-600 font-normal justify-center -top-1">
-                      1
-                    </span>
-                  </button>
-                </div>
-                <div className=" w-[1px] bg-[#DAF1E9] h-[40px]"></div>
-                <div className=" inline-flex items-center gap-5">
-                  <LocaleSwitcher />
-                </div>
-              </div>
-            </li>
-          </ul>
-        )}
+            </ul>
+          )}
+        </div>
       </div>
     </header>
   );

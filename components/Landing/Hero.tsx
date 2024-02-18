17e2f8e3ea92c49509da0d9cddd73810453bcb10
tarifalhasan <tarifalhasan999@gmail.com>
2024-02-18 11:25:12 +0600

    update hero section

--- components/Landing/Hero.tsx
+++ components/Landing/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BiChevronRight } from "react-icons/bi";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -9,7 +10,7 @@ import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 
 // import required modules
-import { EffectFade, Pagination } from "swiper/modules";
+import { Autoplay, EffectFade, Pagination } from "swiper/modules";
 
 export default function Hero() {
   const data = [
@@ -30,37 +31,88 @@ export default function Hero() {
         pagination={{
           clickable: true,
         }}
-        modules={[EffectFade, Pagination]}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        modules={[EffectFade, Pagination, Autoplay]}
         className="heroSwiper"
       >
-        {data.map((item, index) => (
-          <SwiperSlide key={index}>
-            <div
-              className="  relative overflow-hidden min-h-[150px] lg:min-h-[300px] xl:min-h-[350px] 2xl:min-h-[550px] bg-contain block m-0   pb-[30%] bg-no-repeat"
-              style={{
-                backgroundImage: `url('${item.imageUrl}')`,
-                backgroundPosition: "100% 100%",
-                backgroundSize: "100% 100%",
-              }}
-            >
-              {/* <div className="container relative mx-auto py-28 md:py-48 px-4 w-full left-0 top-0">
-                <div className="space-y-7">
-                  <h2 className="font-bold text-center font-sans lg:text-left text-2xl sm:text-4xl xl:text-5xl">
-                    {item.title}
+        <SwiperSlide>
+          <div
+            className="  relative overflow-hidden min-h-[450px] lg:min-h-[300px] xl:min-h-[350px] 2xl:min-h-[550px] bg-contain block m-0   bg-no-repeat"
+            style={{
+              backgroundImage: `url('/images/hero/1.png')`,
+              backgroundPosition: "100% 100%",
+              backgroundSize: "100% 100%",
+            }}
+          >
+            <div className=" container relative w-full h-full min-h-[450px] lg:min-h-[300px] xl:min-h-[350px] 2xl:min-h-[550px]">
+              <div className="container absolute  top-1/2 -translate-y-1/2  mx-auto  px-4 w-full left-0 ">
+                <div className="xl:space-y-7 space-y-3">
+                  <h2 className="font-bold text-center font-sans lg:text-left text-2xl sm:text-4xl xl:text-5xl text-white">
+                    SUNMI hyper Wi-Fi
                   </h2>
-                  <p className="max-w-[542px] mx-auto lg:mx-0 font-sans text-lg text-center lg:text-left xl:text-xl font-bold">
-                    {item.description}
+                  <p className="max-w-[542px] text-white mx-auto lg:mx-0 font-sans text-lg text-center lg:text-left xl:text-xl  font-normal">
+                    Stay connected wherever your business{" "}
+                    <br className=" hidden xl:block" /> takes you
                   </p>
-                  <div className="flex justify-center lg:justify-start">
-                    <Button className="px-6 rounded-full bg-white text-primary hover:text-foreground lg:bg-primary lg:text-white lg:hover:text-white">
-                      เรียนรู้เพิ่มเติม
-                    </Button>
+                  <div className="flex text-white justify-center lg:justify-start">
+                    <button className=" inline-flex items-center  hover:gap-1 transition-all duration-300 hover:text-primary outline-none focus:outline-none">
+                      <span>Learn More</span>{" "}
+                      <BiChevronRight className=" w-7 h-7" />
+                    </button>
                   </div>
                 </div>
-              </div> */}
+              </div>
             </div>
-          </SwiperSlide>
-        ))}
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="  relative overflow-hidden min-h-[450px] lg:min-h-[300px] xl:min-h-[350px] 2xl:min-h-[550px] bg-contain block m-0   bg-no-repeat"
+            style={{
+              backgroundImage: `url('/images/hero/2.jpg')`,
+              backgroundPosition: "100% 100%",
+              backgroundSize: "100% 100%",
+            }}
+          >
+            <div className="container relative w-full h-full min-h-[450px] lg:min-h-[300px] xl:min-h-[350px] 2xl:min-h-[550px]">
+              <div className="container   absolute top-1/2 -translate-y-1/2 mx-auto  px-4 w-full left-0 ">
+                <div className=" space-y-2 xl:space-y-6">
+                  <p className="max-w-[542px] text-white mx-auto lg:mx-0 font-sans text-lg text-center lg:text-left xl:text-xl  font-normal">
+                    Smart Desktop Terminal
+                  </p>
+                  <h2 className="font-bold text-center font-sans lg:text-left text-2xl sm:text-4xl xl:text-5xl text-primary">
+                    SUNMI D3 MINI
+                  </h2>
+                  <p className="max-w-[542px] text-white mx-auto lg:mx-0 font-sans text-lg text-center lg:text-left xl:text-xl  font-normal">
+                    Compact,loaded with features.
+                  </p>
+                  <div className="flex text-white justify-center lg:justify-start">
+                    <button className=" inline-flex items-center  hover:gap-1 transition-all duration-300 hover:text-primary outline-none focus:outline-none">
+                      <span>Learn More</span>{" "}
+                      <BiChevronRight className=" w-7 h-7" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="  relative overflow-hidden min-h-[450px] lg:min-h-[300px] xl:min-h-[350px] 2xl:min-h-[550px] bg-cover bg-center block m-0   bg-no-repeat"
+            style={{
+              backgroundImage: `url('/images/hero/3.png')`,
+              // backgroundPosition: "100% 50%",
+            }}
+          >
+            <div className="container relative w-full h-full min-h-[450px] lg:min-h-[300px] xl:min-h-[350px] 2xl:min-h-[550px]">
+              <div className="container  mb-6  absolute top-1/2 -translate-y-1/2 mx-auto py-28 md:py-[9rem] px-4 w-full left-0 "></div>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </>
   );

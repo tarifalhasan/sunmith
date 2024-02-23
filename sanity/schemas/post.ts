daa6265070037f1eb97321d4b15c8a07b9d43745
tarifalhasan <tarifalhasan999@gmail.com>
2024-02-23 13:44:28 +0600

    update sanity blog?

--- sanity/schemas/post.ts
+++ sanity/schemas/post.ts
@@ -2,7 +2,7 @@ import { defineField, defineType } from "sanity";
 
 export default defineType({
   name: "post",
-  title: "Post",
+  title: "Blog Post",
   type: "document",
   fields: [
     defineField({

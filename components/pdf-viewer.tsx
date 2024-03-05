66742b913ed7a7050555ea6a0749b4f5475d8560
tarifalhasan <tarifalhasan999@gmail.com>
2024-03-05 10:59:39 +0600

    update

--- components/pdf-viewer.tsx
+++ components/pdf-viewer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useResizeObserver } from "@wojtekmaj/react-hooks";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
@@ -31,7 +31,6 @@ export default function ProductDetails({ pdf }: { pdf: string }) {
   const [numPages, setNumPages] = useState<number>();
   const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
   const [containerWidth, setContainerWidth] = useState<number>();
-  const [loading, setLoading] = useState(true);
 
   const onResize = useCallback<ResizeObserverCallback>((entries) => {
     const [entry] = entries;
@@ -49,42 +48,27 @@ export default function ProductDetails({ pdf }: { pdf: string }) {
     setNumPages(nextNumPages);
   }
 
-  useEffect(() => {
-    const delay = 4000; // 1 minute
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, delay);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div className="Example">
-      {loading ? (
-        <div className="loading-indicator">Loading...</div>
-      ) : (
-        <div className="Example__container">
-          <div className="Example__container__document" ref={setContainerRef}>
-            <Document
-              file={file}
-              onLoadSuccess={onDocumentLoadSuccess}
-              options={options}
-            >
-              {Array.from(new Array(numPages), (el, index) => (
-                <Page
-                  key={`page_${index + 1}`}
-                  pageNumber={index + 1}
-                  width={
-                    containerWidth
-                      ? Math.min(containerWidth, maxWidth)
-                      : maxWidth
-                  }
-                />
-              ))}
-            </Document>
-          </div>
+    <div className="PD">
+      <div className="PD__container">
+        <div className="PD__container__document" ref={setContainerRef}>
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={
+                  containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+                }
+              />
+            ))}
+          </Document>
         </div>
-      )}
+      </div>
     </div>
   );
 }

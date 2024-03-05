ee895ca1017fdfc39e78b99460d71abccd485f0c
tarifalhasan <tarifalhasan999@gmail.com>
2024-03-05 11:27:26 +0600

    update product

--- app/[locale]/GlobalProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { CartProvider } from "@/context/cart";
-import { ReactNode } from "react";
-
-export function GlobalProvider({ children }: { children: ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
-}

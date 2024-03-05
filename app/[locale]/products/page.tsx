321496b05096ee7088beccb9cc5ddc2d7852fee2
tarifalhasan <tarifalhasan999@gmail.com>
2024-03-05 14:54:40 +0600

    update product card

--- app/[locale]/products/page.tsx
+++ app/[locale]/products/page.tsx
@@ -64,22 +64,19 @@ const ProductsPage = async () => {
                     </span>
                   </div>
                 </div>
-                <div className=" grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-12">
+                <div className=" grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-12 xl:gap-5">
                   {series.products.map((product, index) => (
                     <div key={product._id || index}>
-                      <div className="p-5 py-10 bg-purple-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
+                      <div
+                        style={{
+                          background: "rgb(240, 240, 240)",
+                        }}
+                        className="p-5  py-10  text-center transform duration-500 hover:-translate-y-2 cursor-pointer"
+                      >
                         <Link
                           className=" block w-full"
                           href={`/products/${product.currentSlug}`}
                         >
-                          {/* <div className=" relative overflow-hidden h-[260px]">
-                            <img
-                              className=" scale-100 transition-all duration-300  hover:scale-110   h-auto block mx-auto "
-                              quality={80}
-                              src={urlForImage(product.mainImage.asset)}
-                              alt={product.mainImage.alt}
-                            />
-                          </div> */}
                           <div className="  flex justify-center w-full">
                             <div className=" w-[10rem] h-[10rem]  flex justify-center">
                               <Image
@@ -93,11 +90,16 @@ const ProductsPage = async () => {
                           </div>
                         </Link>
                         <Link href={`/products/${product.currentSlug}`}>
-                          <h2 className="text-3xl my-5">
+                          <h2 className=" text-base  font-bold lg:text-xl my-5">
                             {product.productTitle}
                           </h2>
                           <p className="mb-2">{product.subTitle}</p>
-                          <h3 className="font-semibold mb-3">
+                          <h3
+                            style={{
+                              color: "rgb(102, 102, 102)",
+                            }}
+                            className="font-semibold text-sm xl:text-base mb-3"
+                          >
                             ${product.price}
                           </h3>
                         </Link>

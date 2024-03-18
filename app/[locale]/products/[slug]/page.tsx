575753bfc2ae36bf49cc4a5a70d79b65b149f98d
tarifalhasan <tarifalhasan999@gmail.com>
2024-03-18 09:11:56 +0600

    changeds

--- app/[locale]/products/[slug]/page.tsx
+++ app/[locale]/products/[slug]/page.tsx
@@ -3,7 +3,6 @@ import getSingleProduct from "@/services/getSingleProduct";
 
 const PDFDJ = async ({ params }: { params: { slug: string } }) => {
   const item = await getSingleProduct(params.slug);
-  console.log(item);
   return (
     <div>
       <ProductDetails pdf={item?.pdfUrl || "/404.pdf"} />
